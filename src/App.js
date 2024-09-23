eeacc8997c90b33ec4b10cb8160ba0092691bb38
geniebaek <baichen99@naver.com>
2024-09-23 17:57:36 +0900

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { DiGithubAlt } from "react-icons/di";
+// import Hello from './01/Hello'; //import Hello.js
+//import MyClock from './02/MyClock'; //import MyClock.js
+import { IoHome } from "react-icons/io5";
+// import Mydiv1 from './03/Mydiv1';
+import Mylist from './04/Mylist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-slate-200'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-          <DiGithubAlt/>
-        </a>
+    <div className="w-full xl:w-10/20 h-screen mx-auto 
+                    flex flex-col justify-center items-center">
+      <header className='w-full h-20
+                         flex justify-between items-center
+                         bg-slate-200'>
+       <p className='text-2xl font-bold p-5'>K-digital 8기</p>
+       <p className='text-4xl font-bold p-5 '><IoHome/></p>
+       
       </header>
+      <main className='w-full grow
+                       flex flex-col justify-center items-center
+                       overflow-y-auto'>
+       {/* <Mydiv1/> */}
+       <Mylist/>
+      </main>
+      <footer className='w-full h-20
+                         flex justify-center items-center
+                         text-white bg-black'>
+      <p>2024 K-digital 8기 지니</p>
+      </footer>
+      
     </div>
   );
 }
