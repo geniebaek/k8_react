45869f10ab14a0d7ba9da105adb782199a20a7ff
geniebaek <baichen99@naver.com>
2024-09-27 17:39:38 +0900

    04

--- src/04/Mylist.js
+++ src/04/Mylist.js
@@ -1,14 +1,16 @@
 import MyListData from './MyListData.json';
 import Mylistitem from './Mylistitem';
 export default function Mylist() {
+  
   console.log(MyListData)
-  MyListData.map(item=>console.log(item.title)  );
+  const tags=MyListData.map(item=><Mylistitem
+                                   key={item.title}
+                                   title={item.title}
+                                   content={item.content}
+                                   imgUrl={item.imgUrl}/> );
   return (
-    <div className='w-10/12 grid grid-cols-2 gap-4'>  
-      <Mylistitem/>
-      <Mylistitem/>
-      <Mylistitem/>
-      <Mylistitem/>
+    <div className='w-10/12  grid grid-cols-1 lg:grid-cols-2 gap-4'>  
+      {tags}
     </div>
   )
 }
--- src/04/Mylistitem.js
+++ src/04/Mylistitem.js
@@ -1,25 +1,36 @@
-
-export default function Mylistitem() {
+import{useState} from'react';
+export default function Mylistitem({title, content,imgUrl}) {
+  const[n, setN] = useState(0);
+  
+  const handleClick = () => {
+    setN(n+1);
+    console.log(n);
+  }
+ 
   return (
-    <div className='w-full
-    border border-gray-300
-    flex justify-center items-center'>
-      <div className='w-1/3 flex justify-center items-start'>
-        이미지
+    <div className='w-full  h-full
+                    border border-gray-300
+                    flex justify-center items-center'>
+      <div className='w-1/3 flex justify-start items-start'>
+        <img src={imgUrl} alt={title} />
       </div>
-      <div className='w-2/3 flex flex-col justify-between items-center'>
-        <div>
-          <div>
-            title
+      <div className='w-2/3 h-full p-5 flex flex-col justify-start items-start'>
+        <div className="flex flex-col h-3/4">
+          <div className='text-2xl font-bold mb-2'>
+            {title}
           </div>
-          <div>
-            content
+          <div className='text-sm'>
+            {content}
           </div>
         </div>
-        <div>
-          <span>❤️</span>
-          <span>좋아요</span>
-          <span>0</span>
+        <div className="flex w-full h-1/4
+                              justify-end items-end">
+          <div className="text-2xl cursor-pointer"
+                 onClick={handleClick}>
+            ❤️
+            </div>
+          <div className="mx-2 font-bold">좋아요</div>
+          <div>{n}</div>
         </div>
       </div>
     </div>
